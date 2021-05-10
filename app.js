5d7c121e042cbc087f10f2a7b918731eeb0d55f8
Yeferson Tapias Ramirez <tapiasyeferson@gmail.com>
2021-05-10 15:16:32 -0500

    Variables: var, let and const

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 document.write ("Codice Practice on the console");
 
-// Data type
+// Data types
+
+
 // Strings
 "First codice from practice"
 'Second codice from practice'
+
 
 // Number 
 1000
 1.000
 -1.000
 
+
 // Boolean
 true
 false
+
 
 // Array
 ['joe', 'ryan', 'scoot'];
 [1, 2, 3];
 [true, false, true, false];
+
 
 // Objects
 
@@ -34,3 +40,9 @@ false
     "hours": 2,
     "proffesional": false
 });
+
+
+// Variables
+var username = "ryan";
+let username = "jeff"
+const lastname = "carter"
